246471b21c549792efed186a289a5fb1401c8e30
Kavan Pancholi <kavanpancholi@gmail.com>
2016-10-15 01:34:19 +0530

    tweet stream works, job queue and tweets entry in db works

---
+++ app/TwitterStream.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use OauthPhirehose;
+use App\Jobs\ProcessTweet;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+
+
+class TwitterStream extends OauthPhirehose
+{
+	use DispatchesJobs;
+
+    public function enqueueStatus($status)
+    {
+
+        $this->dispatch(new ProcessTweet($status));
+    }
+}
