e8129cdd71e71f0f73a21cab7376fee3a8713aec
Kavan Pancholi <kavanpancholi@gmail.com>
2016-10-15 02:11:27 +0530

    changed app name, fix tweet link

--- resources/views/tweets/tweet.blade.php
+++ resources/views/tweets/tweet.blade.php
@@ -5,7 +5,7 @@
     <div class="media-body">
         <h4 class="media-heading">{{ '@' . $tweet->user_screen_name }}</h4>
         <p>{{ $tweet->tweet_text }}</p>
-        <p><a target="_blank" href="https://twitter.com/{{ $tweet->user_screen_name }}/status/{{ $tweet->id }}">
+        <p><a target="_blank" href="https://twitter.com/{{ $tweet->user_screen_name }}/status/{{ $tweet->tweet_id }}">
             View on Twitter
         </a></p>
     </div>
