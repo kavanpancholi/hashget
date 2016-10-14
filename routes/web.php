8766c6e15372ce42636064d687f082b56e1a84e3
Kavan Pancholi <kavanpancholi@gmail.com>
2016-10-15 02:05:25 +0530

    added authentication, implemented approve/disapprove functionality

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,29 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if (Auth::check()) {
+        $tweets = App\Tweet::orderBy('created_at','desc')->paginate(5);
+    } else {
+        $tweets = App\Tweet::where('approved',1)->orderBy('created_at','desc')->take(5)->get();
+    }
+
+    return view('welcome', ['tweets' => $tweets]);
 });
+
+Route::post('approve-tweets', ['middleware' => 'auth', function (Illuminate\Http\Request $request) {
+    foreach ($request->all() as $input_key => $input_val) {
+        if ( strpos($input_key, 'approval-status-') === 0 ) {
+            $tweet_id = substr_replace($input_key, '', 0, strlen('approval-status-'));
+            $tweet = App\Tweet::where('id',$tweet_id)->first();
+            if ($tweet) {
+                $tweet->approved = (int)$input_val;
+                $tweet->save();
+            }
+        }
+    }
+    return redirect()->back();
+}]);
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
